c058990c465ae71f3ac9230bccbff487c1fa0153
mathieuMarthy <mathieu.marthy@gmail.com>
2025-10-19 15:36:30 +0200

    send notification to reconnect when the token is expired

--- src/dao/cloudDAO.py
+++ src/dao/cloudDAO.py
@@ -10,5 +10,14 @@ class CloudDAO(ABC):
     def download_files(self):
         pass
 
-    def init_connection(self):
+    def init_connection(self, can_open_connection_page: bool = False):
+        """Establishes a connection to the cloud service and save credentials locally.
+
+        Args:
+            can_open_connection_page (bool): If True, the method can open the browser
+                to complete the OAuth2 authentication flow. If False, it will raise an exception
+                if no valid credentials are found.
+        Raises:
+            DaoConnectionException: If authentication is required but can_open_connection_page is False.
+        """
         pass
---
+++ src/services/NotificationService.py
@@ -0,0 +1,24 @@
+import logging
+from typing import Callable
+
+from desktop_notifier import DesktopNotifier, Button
+
+from src.models.sync_parameters import CloudProvider
+
+
+class NotificationService:
+    notifier = DesktopNotifier(app_name="Sync Files")
+
+    @staticmethod
+    async def send_reconnection_notification(cloud_provider: CloudProvider, reconnect_function: Callable = None):
+        logging.info(f"Sending reconnection notification for {cloud_provider.value}")
+        await NotificationService.notifier.send(
+            title="Reconnection Required",
+            message=f"Please reconnect to {cloud_provider.value} to continue syncing files.",
+            buttons=[
+                Button(
+                    title="Reconnect",
+                    on_pressed=reconnect_function,
+                )
+            ]
+        )
